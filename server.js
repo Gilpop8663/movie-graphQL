57125ce097d986a77f8ce24c65fd43ca312b7d2b
gilpop8663 <wolfye@naver.com>
2023-04-14 05:40:34 +0900

    feat: 영화 정보 API를 받아와 graphQL로 반환하는 기능 구현

--- server.js
+++ server.js
@@ -1,12 +1,75 @@
 import { ApolloServer, gql } from 'apollo-server';
+import fetch from 'node-fetch';
 
 const typeDefs = gql`
+  type Torrents {
+    url: String
+    hash: String
+    quality: String
+    type: String
+    seeds: Int
+    peers: Int
+    size: String
+    size_bytes: Int
+    date_uploaded: String
+    date_uploaded_unix: Int
+  }
+
+  type Movie {
+    id: Int
+    url: String
+    imdb_code: String
+    title: String
+    title_english: String
+    title_long: String
+    slug: String
+    year: Int
+    rating: Float
+    runtime: Int
+    summary: String
+    description_full: String
+    synopsis: String
+    yt_trailer_code: String
+    language: String
+    mpa_rating: String
+    background_image: String
+    background_image_original: String
+    small_cover_image: String
+    medium_cover_image: String
+    large_cover_image: String
+    state: String
+    date_uploaded: String
+    date_uploaded_unix: Int
+    torrents: [Torrents]
+    genres: [String]
+  }
+
   type Query {
-    text: String
+    allMovies: [Movie]
+    movie: Movie
   }
 `;
 
-const server = new ApolloServer({ typeDefs });
+const resolvers = {
+  Query: {
+    async allMovies() {
+      const fetchedData = await fetch(
+        'https://yts.mx/api/v2/list_movies.json'
+      ).then(response => response.json());
+
+      return fetchedData.data.movies;
+    },
+    async movie(root, { id }) {
+      const fetchedData = await fetch(
+        `https://yts.mx/api/v2/movie_details.json?movie_id=${id}`
+      ).then(response => response.json());
+
+      return fetchedData.data.movie;
+    },
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
 
 server.listen().then(({ url }) => {
   console.log(`Running on ${url}`);
